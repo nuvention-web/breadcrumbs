35c92de720636e5365ec1d2c4f47b74574228a91
aspin <tsunami70875@gmail.com>
2015-04-27 22:35:52 -0500

    link fix

--- chrome-extension/app/scripts/popup.js
+++ chrome-extension/app/scripts/popup.js
@@ -2,7 +2,7 @@
 
 var id;
 // var ceres = new Asteroid('breadcrumbs.meteor.com');
-var ceres = new Asteroid('localhost:3000');
+var ceres = new Asteroid('breadcrumbs.ninja');
 
 // on start, check if authenticated
 chrome.storage.local.get('breadcrumbsID', function(items) {
