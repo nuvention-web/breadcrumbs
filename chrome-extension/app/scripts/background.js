42573e98e6912d7d132799cd29d27b32e194327b
aspin <tsunami70875@gmail.com>
2015-04-28 12:14:39 -0500

    fix

--- chrome-extension/app/scripts/background.js
+++ chrome-extension/app/scripts/background.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* POST REQUEST SETUP */
-var domain = 'http://localhost:3000';
-// var domain = 'http://breadcrumbs.ninja';
+// var domain = 'http://localhost:3000';
+var domain = 'http://breadcrumbs.ninja';
 var path = domain + '/datapost';
 var tabStore = {};
 
